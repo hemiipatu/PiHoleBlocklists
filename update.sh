d2d736dbd648e00e4a03d357c88c77e9ca651153
Hemi <16671116+hemiipatu@users.noreply.github.com>
2022-02-10 20:18:05 +0000

    update update.sh.
    Fixed an incorrect reference used on line 15.

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ fi
 # cat reads the contents of /etc/pihole/custom-adlists.list (A file which you create that just has blocklist URL's in it)
 # cat also receives input from wget as the contents of wget is being thrown into cat as a "file"
 # This ensures that one big list of blocklist URL's are being piped to tee with admin privledges into /etc/pihole/adlists.list.
-cat /etc/pihole/custom-adlists.list <(wget -qO - https://raw.githubusercontent.com/hemiipatu/PiHoleBlocklists/master/blocklists-update/ultimate-list.txt 2> /dev/null) | sudo tee /etc/pihole/adlists.list
+cat /etc/pihole/custom-adlists.list <(wget -qO - https://raw.githubusercontent.com/hemiipatu/PiHoleBlocklists/master/blocklists-update/ultimate-list.txt 2> /dev/null) | sudo tee /etc/pihole/custom-adlists.list
 
 # Update the adlists
 pihole -g -f
